1d43c75c8fff044609305dfd78df9abb9a17e595
ozlemyalcin <omersam33@gmail.com>
2020-12-11 10:28:59 -0600

    angular part-2 finish

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,6 +10,7 @@ export class HeaderComponent implements OnInit {
   missionName: string = "Mars 2030";
   rocketName: string = "Plasma Max";
   editingMission: boolean = false;
+  editingRocket: boolean = false;
   
   constructor() { }
 
@@ -21,4 +22,13 @@ export class HeaderComponent implements OnInit {
     this.editingMission = false;
   }
 
+  updateRocket(newRocketName: string){
+    this.rocketName= newRocketName;
+    this.editingRocket = false;
+
+  }
+
+  
+
+  
 }
