52f0350621457e4e8ae0f858274a431cb0702ea5
ozlemyalcin <omersam33@gmail.com>
2020-12-15 15:21:27 -0600

    continue

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -27,5 +27,15 @@ export class EquipmentComponent implements OnInit {
    ngOnInit() { }
 
    // Code your addItem function here:
+   addItem(equipment:object):boolean{
+      this.cargoHold.push(equipment);
+      this.cargoMass += this.cargoHold['mass'];
+      // check if else block ???
+      if(this.cargoMass<this.maximumAllowedMass && this.cargoMass>=200){
+        return true;
+      }else{
+        return false;
+      }
+   }
    
 }
